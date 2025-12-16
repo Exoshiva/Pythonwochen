e630e288b6128df77075114062775bdd4f85e8c0
Lars Patzenbein <exoshiva25@gmail.com>
2025-12-16 08:40:37 +0100

    refactor: Fehler-Keys im API-Return auf 'error' vereinheitlicht

--- TestProject6/api_handler.py
+++ TestProject6/api_handler.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 # 1. Umgebungsvariablen aus der .env laden (mit dotenv_load)
 load_dotenv() 
 
@@ -13,8 +14,8 @@ def get_weather_data(city):
         Diese Funktion ruft die Wetterdaten ab und gibt sie zurück.
     """
     if not city:
-        return "Bitte einen Stadt eingeben"
-
+        return None # NEU: Rückgabe von nichts bei leerer Eingabe
+    
     # 3. API-URL aufbauen (Metrisch für Celsius, lang=de für deutsche Texte)
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric" 
     
@@ -27,20 +28,21 @@ def get_weather_data(city):
         # 4. JSON Daten auspacken
         data = response.json()
         
-        # Daten extrahieren (Parsing)
-        temp = data['main']['temp']
-        desc = data['weather'][0]['description']
-        
-        # Einen schönen Text zurückgeben
-        return f"Wetter in {city}: \nTemperatur:{temp}°C\nBeschreibung:{desc}"    
+        # Jetzt das Dictonary aufrufen
+        return {
+            "stadt": city,
+            "temp": data['main']['temp'],
+            "desc": data['weather'][0]['description'],
+            "error": None # Kein Fehler (Anfrage wird gestartet)
+        }
     
     except requests.exceptions.HTTPError:  
         # Eine spezielle Meldung wenn Stadt nicht gefunden wurde (404)
-        return "Fehler: Stadt wurde nicht gefunden" 
+        return {"error": "Die Stadt wurde nicht gefunden"} 
     except requests.exceptions.ConnectionError:
         # Fehlermeldung wenn keine Internetverbindung besteht
-        return "Fehler: Keine Internetverbindung"
-    
+        return {"error": "Keine Internetverbindung"}
+   
     except Exception as e:
         # Dies gibt alle anderen Fehler aus
-        return f"Allgemeiner Fehler: {e}"
+        return {"error": f"Fehler: {e}"}

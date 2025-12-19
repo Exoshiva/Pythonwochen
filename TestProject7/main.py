b58feaf29cbcf341c983e25d5e49d8f15b210536
Lars Patzenbein <exoshiva25@gmail.com>
2025-12-19 09:23:45 +0100

    feat: add security pipeline

--- TestProject7/main.py
+++ TestProject7/main.py
@@ -1,1 +1,13 @@
-print('Hallo aus TestProject7')
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hallo Ramblr.ai! Ich bin ein DevSecOps Beispiel.!"
+
+if __main__ == '__main__':
+    # Wichtig: degug=False für Produktion!
+    # Ein Security-Scanner würde hier meckern, wenn hier True stünde.
+    app.run(host='0.0.0.0.', port=5000, debug=False)
